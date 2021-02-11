41e88ce13ced5b1b29be0d7e28070bfd39713873
Pamela Chen <pamelakaylin@gmail.com>
2021-02-11 10:34:42 +0000

    mostly done, finished contact for mobile

--- public/main.js
+++ public/main.js
@@ -22,53 +22,109 @@ $(() => {
     $(this).css('background-color', color);
   })
 
-  const contentList = [
-    'Pamela Kaylin', 
-    'Full-stack engineer', 
-    'Welcome to my space', 
-    'I create my own world.'
-  ];
+  function titleTextAnimation () {
+    const contentList = [
+      'Pamela Kaylin', 
+      'Full-stack engineer', 
+      'Welcome to my space', 
+      'I create my own world.'
+    ];
 
-  let text = $('#type-text')[0];
-  let intervalFunc;
-  let sentence = 0;
-  let index = 0;
+    let text = $('#type-text')[0];
+    let intervalFunc;
+    let sentence = 0;
+    let index = 0;
+    $('.text-container').css('text-decoration', 'none')
 
-  $('.text-container').css('text-decoration', 'none')
+    function typeText () {
+      let letter = contentList[sentence][index];
+      text.innerHTML += letter;
+      index++;
+  
+      if (text.innerHTML === contentList[sentence]) {
+        clearInterval(intervalFunc);
+        if (sentence < contentList.length-1) {
+          setTimeout(() => {
+            intervalFunc = setInterval(deleteText, 50);
+          }, 1000);
+        }
+      }
+    }
 
-  function typeText () {
-    let letter = contentList[sentence][index];
-    text.innerHTML += letter;
-    index++;
-
-    if (text.innerHTML === contentList[sentence]) {
-      clearInterval(intervalFunc);
-      if (sentence < contentList.length-1) {
+    function deleteText () {
+      let written = text.innerHTML.substring(0,index-1);
+      text.innerHTML = written;
+      index--;
+  
+      if (written === '') {
+        clearInterval(intervalFunc);
+        sentence++;
         setTimeout(() => {
-          intervalFunc = setInterval(deleteText, 50);
+          intervalFunc = setInterval(typeText, 100);
+        },500);
+        
+      }
+    }
+    
+    setTimeout(() => {
+      intervalFunc = setInterval(typeText, 100);
+    }, 1000);
+  }
+
+  titleTextAnimation ();
+
+  function langTextAnimation () {
+    const langList = [
+      'English.', 
+      'Mandarin', 
+      'Spanish.'
+    ];
+
+    let languageText = $('#lang-option')[0];
+    let intervalFunc;
+    let language = 0;
+    let index = 0;
+
+    function typeLang () {
+      let letter = langList[language][index];
+      languageText.innerHTML += letter;
+      index++;
+  
+      if (languageText.innerHTML === langList[language]) {
+        clearInterval(intervalFunc);
+        setTimeout(() => {
+          intervalFunc = setInterval(deleteLang, 50);
         }, 1000);
       }
     }
-  }
 
-  function deleteText () {
-    let written = text.innerHTML.substring(0,index-1);
-    text.innerHTML = written;
-    index--;
-
-    if (written === '') {
-      clearInterval(intervalFunc);
-      sentence++;
-      setTimeout(() => {
-        intervalFunc = setInterval(typeText, 100);
-      },500);
-      
+    function deleteLang () {
+      let written = languageText.innerHTML.substring(0,index-1);
+      languageText.innerHTML = written;
+      index--;
+  
+      if (written === '') {
+        clearInterval(intervalFunc);
+        if (language === langList.length-1) language = 0;
+        else language++;
+        setTimeout(() => {
+          intervalFunc = setInterval(typeLang, 100);
+        },500);
+        
+      }
     }
+
+    setTimeout(() => {
+      intervalFunc = setInterval(typeLang, 100);
+    }, 1000);
+
   }
 
-  setTimeout(() => {
-    intervalFunc = setInterval(typeText, 100);
-  }, 1000);
+  langTextAnimation ();
+
+
+
+
 
 
 
